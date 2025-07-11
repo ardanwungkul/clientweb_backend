96c07c68a18834f44deb0aa0f6006f1b0261ab2f
ardanwungkul <ardanwungkul14@gmail.com>
2025-07-11 11:06:14 +0700

    options api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,29 +17,29 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make(12345678),
-        //     'role' => 'admin'
-        // ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make(12345678),
+            'role' => 'admin'
+        ]);
         // User::factory()->create([
         //     'name' => 'Ardan',
         //     'email' => 'ardanwungkul143@gmail.com',
         //     'password' => Hash::make(12345678),
         //     'role' => 'karyawan'
         // ]);
-        User::factory()->create([
-            'name' => 'Arya',
-            'email' => 'arya@gmail.com',
-            'password' => Hash::make(12345678),
-            'role' => 'karyawan'
-        ]);
-        User::factory()->create([
-            'name' => 'Kurnia',
-            'email' => 'kurnia@gmail.com',
-            'password' => Hash::make(12345678),
-            'role' => 'pkl'
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Arya',
+        //     'email' => 'arya@gmail.com',
+        //     'password' => Hash::make(12345678),
+        //     'role' => 'karyawan'
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'Kurnia',
+        //     'email' => 'kurnia@gmail.com',
+        //     'password' => Hash::make(12345678),
+        //     'role' => 'pkl'
+        // ]);
     }
 }
