39f58f06bc90361538dafc6cc08d20123d72496b
danyzahir <danyit1b@gmail.com>
2025-07-22 15:08:37 +0700

    pelanggan

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Pelanggan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'nama_pelanggan',
+        'alamat',
+        'no_hp',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
